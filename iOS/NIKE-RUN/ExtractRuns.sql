17ca2c6fd5a7fd56ed69244477356b0addf56b14
Brigs <abrignoni@gmail.com>
2018-10-25 12:22:33 -0400

    Update ExtractRuns.sql

--- iOS/NIKE-RUN/ExtractRuns.sql
+++ iOS/NIKE-RUN/ExtractRuns.sql
@@ -1,4 +1,4 @@
 SELECT lat.sourceid, datetime(lat.startDateInUtcSeconds, 'unixepoch', 'localtime') as day, lat.startDateInUtcSeconds, lat.value as latitude, long.value as long
 FROM metrics as lat left join metrics as long on lat.startDateInUtcSeconds = long.startDateInUtcSeconds
-WHERE lat.type = 'latitude' and long.type = 'longitude' and lat.sourceID = integer_value_of_interest
+WHERE lat.type = 'latitude' and long.type = 'longitude'
 ORDER BY lat.startDateInUtcSeconds
