4492e6da1182111e2e60bbd5417dd25c04d06dbb
Brigs <abrignoni@gmail.com>
2018-11-10 19:04:58 -0500

    Update TikTokUserData.sql

--- iOS/TIKTOK/TikTokUserData.sql
+++ iOS/TIKTOK/TikTokUserData.sql
@@ -1,6 +1,6 @@
 /* The database needed for this query is called AwemeIM.db.
 This query was tested in DB Browser for SQLite verion 3.10.1. 
-For details see blog post here:
+For details see blog post here: https://abrignoni.blogspot.com/2018/11/finding-tiktok-messages-in-ios.html
 By: Alexis Brignoni
 Twitter: @AlexisBrignoni
 */
