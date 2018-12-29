403a3b2bf57115f3c1df952e130fcd8aa6e60468
Brigs <abrignoni@gmail.com>
2018-12-28 23:36:10 -0500

    Update iOS_UninstalledApps.sql

--- iOS/INSTALLED-UNINSTALLED-APPS/iOS_UninstalledApps.sql
+++ iOS/INSTALLED-UNINSTALLED-APPS/iOS_UninstalledApps.sql
@@ -1,8 +1,11 @@
 SELECT
 application_identifier_tab.[id],
 application_identifier_tab.[application_identifier]
-FROM application_identifier_tab
-WHERE NOT EXISTS ( SELECT * 
+FROM application_identifier_tab, kvs
+where NOT EXISTS ( SELECT *
 				   FROM 
 				   KVS
 				   WHERE (application_identifier_tab.[id] = kvs.[application_identifier]))
+				   
+OR (kvs.[key] = 13 and (application_identifier_tab.[id] = kvs.[application_identifier]))
+GROUP BY application_identifier_tab.[id]
