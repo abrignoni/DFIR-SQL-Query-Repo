fee28e50343bc7ab1da07d4000883f8306544364
Brigs <abrignoni@gmail.com>
2018-12-30 18:28:40 -0500

    Delete iOS_UninstalledAppsThatHaveDeletionDates.sql

--- iOS/INSTALLED-UNINSTALLED-APPS/iOS_UninstalledAppsThatHaveDeletionDates.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-application_identifier_tab.[id],
-application_identifier_tab.[application_identifier],
-kvs.[value]
-FROM application_identifier_tab, kvs
-where (kvs.[key] = 13 and (application_identifier_tab.[id] = kvs.[application_identifier]))
