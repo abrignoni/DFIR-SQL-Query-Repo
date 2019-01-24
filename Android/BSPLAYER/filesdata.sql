afa37ec3dea6f638acb1c041e89d50c7c39a3e14
Brigs <abrignoni@gmail.com>
2019-01-23 22:53:48 -0500

    Update filesdata.sql

--- Android/BSPLAYER/filesdata.sql
+++ Android/BSPLAYER/filesdata.sql
@@ -2,10 +2,10 @@ select
 files.name,
 files.path,
 files.numplay,
-files.lastplay,
+datetime(files.lastplay, "unixepoch", "localtime") as lastplay,
 files.fsize / 1e+6 as fsinMB,
 files.mlength / 60000 as mlinMin,
 files.position / 60000 as posMin,
-files.fdate 
+datetime(files.fdate / 1000, "unixepoch", "localtime") as fdate
 from files 
 order by files.numplay desc, files.lastplay
